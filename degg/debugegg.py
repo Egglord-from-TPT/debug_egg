cd91c1f29a71015cdbd2e23bdd0f51014421f0a1
Egglord-from-TPT <karlamongus6767@gmail.com>
2026-03-08 17:04:21 +0200

    Update debugegg.py

--- degg/debugegg.py
+++ degg/debugegg.py
@@ -1,8 +1,13 @@
 import inspect
 import colorama
+import time
 colorama.init(autoreset=True)
+degg_msg=True
 def debug_egg(txt,col="RED"):
     frame=inspect.stack()[1]
     print("[Line: "+str(frame.lineno)+"]   "+getattr(colorama.Fore,col.upper(),"")+str(txt))
     return"[Line: "+str(frame.lineno)+"]   "+getattr(colorama.Fore,col.upper(),"")+str(txt)
 degg=debug_egg
+time.sleep(0.5)
+if degg_msg==True:
+    debug_egg("Hello! It seems you have imported my package 'debug_egg' or 'degg'. I hope you enjoy! \n degg('YOUR_TEXT', 'YOUR_COLOR') \n          -Egglord \n \n","blue")
