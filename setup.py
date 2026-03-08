ec1a428e84e6fc1c836f705f9418d33c16361b71
Egglord-from-TPT <karlamongus6767@gmail.com>
2026-03-08 17:08:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="debugegg",
-    version="1.0.6",
+    version="1.1.0",
     packages=find_packages(),
     description="A simple python package for debugging.",
     author="Egglord",
