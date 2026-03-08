7fd91f871baa35151c24f79d9270dd4039395dff
Egglord-from-TPT <karlamongus6767@gmail.com>
2026-03-08 03:23:25 +0200

    Create __init__.py

---
+++ degg/__init__.py
@@ -0,0 +1,3 @@
+from .debugegg import debug_egg
+if __name__ == "__main__":
+  print("Hello! It seems you have installed my package 'debug_egg' or 'degg'. I hope you enjoy! \n degg('YOUR_TEXT', 'YOUR_COLOR') \n          -Egglord")
