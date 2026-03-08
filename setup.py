545fbca3b78542726f707b8f394740ae86eac6ff
Egglord-from-TPT <karlamongus6767@gmail.com>
2026-03-08 03:39:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="debugegg",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     description="A simple python package for debugging.",
     author="Egglord",
     python_requires=">=3.7",
     install_requires=[
-        "colorama>=0.4.7",
+        "colorama>=0.4.6",
     ]
 )
