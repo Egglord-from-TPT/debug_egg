98c63984af7634cab4f9a5653cd5ab7104231cf7
Egglord-from-TPT <karlamongus6767@gmail.com>
2026-03-08 03:51:29 +0200

    Update __init__.py

--- degg/__init__.py
+++ degg/__init__.py
@@ -1,2 +1,2 @@
-from .debugegg import debug_egg
+from debug_egg.debugegg import debug_egg
 print("Hello! It seems you have installed my package 'debug_egg' or 'degg'. I hope you enjoy! \n degg('YOUR_TEXT', 'YOUR_COLOR') \n          -Egglord")
